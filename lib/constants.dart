9e62b7eee9ed296dab5199c6fd494b3a3c147b89
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2023-07-06 03:46:33 +0530

    Commit 1

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+class ApiConstants {
+  static String baseUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=889eac30334c9045c3bf7a0bc115e999&units=metric';
+  static String usersEndpoint = '/users';
+}
