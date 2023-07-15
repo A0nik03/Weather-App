5bf161203e167904fba42bad3df44019cd2e88b3
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2023-07-15 13:54:59 +0530

    Commit 15-07

--- lib/constants.dart
+++
@@ -1,4 +0,0 @@
-class ApiConstants {
-  static String baseUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=889eac30334c9045c3bf7a0bc115e999&units=metric';
-  static String usersEndpoint = '/users';
-}
--- lib/fetch.dart
+++ lib/fetch.dart
@@ -1,21 +1,25 @@
-import 'dart:developer';
+import 'dart:convert';
+
+
+import 'package:api/Models/Weathh.dart';
 import 'package:http/http.dart' as http;
 
-import 'Models/UsersModel.dart';
-import 'constants.dart';
 
 
-class ApiService {
-  Future<List<UserModel>?> getUsers() async {
-    try {
-      var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
-      var response = await http.get(url);
-      if (response.statusCode == 200) {
-        List<UserModel> _model = userModelFromJson(response.body);
-        return _model;
-      }
-    } catch (e) {
-      log(e.toString());
+
+
+class WeatherData {
+  static Future<Weathh> getData() async {
+    String apiUrl = "api.openweathermap.org/data/2.5/weather?q=Aligarh,India&APPID=889eac30334c9045c3bf7a0bc115e999&units=metric";
+    var url = Uri.parse(apiUrl);
+    var response = await http.get(url);
+    var data = jsonDecode(response.body);
+    if(response.statusCode == 200){
+      return Weathh.fromJson(data);
     }
+    else{
+      return Weathh.fromJson(data);
+    }
+
   }
 }
